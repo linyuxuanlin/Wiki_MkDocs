36ea35fc509a65c1e471bb6c48d296de7fb739bb
linyuxuanlin <824676271@qq.com>
2023-09-25 03:26:30 +0800

    3

--- translate.py
+++ translate.py
@@ -1,47 +1,47 @@
 import os
 import openai
 
-# 设置OpenAI API Key和API Base
+# Set OpenAI API Key and API Base
 openai.api_key = os.environ.get('CHATGPT_API_KEY')
 openai.api_base = os.environ.get('CHATGPT_API_BASE')
 
-# 定义翻译函数
+# Define the translation function
 def translate_file(input_file, output_file):
-    # 读取输入文件内容
+    # Read the content of the input file
     with open(input_file, "r", encoding="utf-8") as f:
         input_text = f.read()
         #print(input_text)
 
-    # 使用OpenAI API进行翻译
+    # Use OpenAI API to translate
     completion = openai.ChatCompletion.create(
         model="gpt-3.5-turbo", messages=[{"role": "user", "content": "Translate the following Chinese text into English:\n\n{}\n\nEnglish:".format(input_text)}]
     )
 
-    # 获取翻译结果
+    # Get the translation result
     output_text = completion.choices[0].message.content
 
-    # 写入输出文件
+    # Write to the output file
     with open(output_file, "w", encoding="utf-8") as f:
         f.write(output_text)
 
-# 遍历docstest目录下的所有.md文件，并进行翻译
+# Translate all .md files in the docstest directory
 for filename in os.listdir("docstest"):
     if filename.endswith(".md"):
-        # 获取文件名和扩展名
+        # Get the file name and extension
         file_name, file_ext = os.path.splitext(filename)
 
-        # 对文件名进行翻译
+        # Translate the file name
         completion = openai.ChatCompletion.create(
             model="gpt-3.5-turbo", messages=[{"role": "user", "content": "Translate the following Chinese text into English:\n\n{}\n\nEnglish:".format(filename)}]
         )
 
-        # 获取翻译结果
+        # Get the translation result
         file_name_en = completion.choices[0].message.content
 
-        # 创建新文件
+        # Create a new file
         output_file = os.path.join("docstest", "en", f"{file_name_en}")
 
-        # 进行文件内容翻译
+        # Translate the content of the file
         input_file = os.path.join("docstest", filename)
         translate_file(input_file, output_file)
 
