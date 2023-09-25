c559c16ece41e0601b177fe0a247b2885e17697e
linyuxuanlin <824676271@qq.com>
2023-09-25 10:00:08 +0800

    2

--- translate.py
+++ translate.py
@@ -5,44 +5,29 @@ import openai
 openai.api_key = os.environ.get('CHATGPT_API_KEY')
 openai.api_base = os.environ.get('CHATGPT_API_BASE')
 
-# Define the translation function
+# 定义翻译函数
 def translate_file(input_file, output_file):
-    # Read the content of the input file
+    # 读取输入文件内容
     with open(input_file, "r", encoding="utf-8") as f:
         input_text = f.read()
         #print(input_text)
 
-    # Use OpenAI API to translate
+    # 使用OpenAI API进行翻译
     completion = openai.ChatCompletion.create(
         model="gpt-3.5-turbo", messages=[{"role": "user", "content": "Translate the following Chinese text into English:\n\n{}\n\nEnglish:".format(input_text)}]
     )
 
-    # Get the translation result
+    # 获取翻译结果
     output_text = completion.choices[0].message.content
 
-    # Write to the output file
+    # 写入输出文件
     with open(output_file, "w", encoding="utf-8") as f:
         f.write(output_text)
 
-# Translate all .md files in the docstest directory
-for filename in os.listdir("docstest"):
+# 遍历docs目录下的所有.md文件，并进行翻译
+for filename in os.listdir("docs"):
     if filename.endswith(".md"):
-        # Get the file name and extension
-        file_name, file_ext = os.path.splitext(filename)
-
-        # Translate the file name
-        completion = openai.ChatCompletion.create(
-            model="gpt-3.5-turbo", messages=[{"role": "user", "content": "Translate the following Chinese text into English:\n\n{}\n\nEnglish:".format(filename)}]
-        )
-
-        # Get the translation result
-        file_name_en = completion.choices[0].message.content
-
-        # Create a new file
-        output_file = os.path.join("docstest", "en", f"{file_name_en}")
-
-        # Translate the content of the file
-        input_file = os.path.join("docstest", filename)
+        input_file = os.path.join("docs", filename)
+        output_file = os.path.join("docs", "en", filename)
         translate_file(input_file, output_file)
-
         print(f"Translated {input_file} to {output_file}")
