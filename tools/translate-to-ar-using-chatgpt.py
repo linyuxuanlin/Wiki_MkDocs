e3d5a5a85ec86e9ee6b191c66a8e33a964aed9b1
linyuxuanlin <824676271@qq.com>
2023-10-07 23:53:02 +0800

    test

--- tools/translate-to-ar-using-chatgpt.py
+++ tools/translate-to-ar-using-chatgpt.py
@@ -149,20 +149,27 @@ file_list = os.listdir(dir_to_translate)
 sorted_file_list = sorted(file_list)
 #print(sorted_file_list)
 
+try:
 # 遍历目录下的所有.md文件，并进行翻译
-for filename in sorted_file_list:
-    if filename.endswith(".md"):
-        input_file = os.path.join(dir_to_translate, filename)
-        output_file = os.path.join(dir_translated, filename)
-        with open(input_file, 'r', encoding='utf-8') as f:
-            md_content = f.read()
-        if "> This post is only available in English." in md_content:
-            md_content.replace("> This post is only available in English.","")
-            print("Translating: ", filename)
-            translate_file(input_file, output_file)
-        elif filename=="index.md" or filename=="Contact-and-Subscribe.md" or filename=="WeChat.md":
-            #os.remove(input_file)
-            print("Pass the post: ", filename)
-        else:
-            print("Translating: ", filename)
-            translate_file(input_file, output_file)
+    for filename in sorted_file_list:
+        if filename.endswith(".md"):
+            input_file = os.path.join(dir_to_translate, filename)
+            output_file = os.path.join(dir_translated, filename)
+            with open(input_file, 'r', encoding='utf-8') as f:
+                md_content = f.read()
+            if "> This post is only available in English." in md_content:
+                md_content.replace("> This post is only available in English.","")
+                print("Translating: ", filename)
+                translate_file(input_file, output_file)
+            elif filename=="index.md" or filename=="Contact-and-Subscribe.md" or filename=="WeChat.md":
+                #os.remove(input_file)
+                print("Pass the post: ", filename)
+            else:
+                print("Translating: ", filename)
+                translate_file(input_file, output_file)
+except Exception as e:
+    # 捕获异常并输出错误信息
+    print(f"发生了异常：{e}")
+    # 可选：在这里添加其他处理异常的代码
+    # 最后，可以选择终止程序
+    raise SystemExit(1)  # 1 表示非正常退出，可以根据需要更改退出码
